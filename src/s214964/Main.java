93f2c95fc206dea74dfad768c5c3700a92132278
Victor Hyltoft <63429024+victorhyltoft@users.noreply.github.com>
2021-09-26 20:02:06 +0200

    Initial commit

---
+++ src/s214964/Main.java
@@ -0,0 +1,10 @@
+package s214964;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("TEST");
+        System.exit(5);
+    }
+}
