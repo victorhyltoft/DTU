7998196f1f7c6687115411600421a56026e822bf
Victor Hyltoft <63429024+victorhyltoft@users.noreply.github.com>
2021-10-01 23:57:25 +0200

    Performance and readability improvements

--- src/assignments/assignment1/PredatorPray.java
+++ src/assignments/assignment1/PredatorPray.java
@@ -4,42 +4,36 @@ import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
+ * @deprecated !!! THIS CLASS IS DEPRECATED !!!
  * @author Victor Hyltoft - s214964
- * 
+ *
  */
 
 public class PredatorPray {
-	
+
 	public static void main(String[] args) {
-		
+
 		runSimulation(23, 3, 100);
 	}
-	
-	// TODO convert array to Point
 
 	/**
 	 * runSimulation
-	 * TODO describe
 	 * @param n describes the grid size height and with
-	 * @param
 	 */
 	public static void runSimulation(int n, int s, int t) {
 
 		// Initialize immutable objects (constants) to prevent accidental altering.
-		final int maxBorder = n;
-		final int minBorder = 1;
-		final int maxMoveLength = s;
-		final int iterations = t;
+        final int minBorder = 1;
 
-		// check parameters are valid.
-		parameterValidation(maxBorder, maxMoveLength, iterations);
+        // check parameters are valid.
+		parameterValidation(n, s, t);
 
 		// Initialize array to store the animals' location
 		int[] pray = {0, 0}, predator = {0, 0};
 
 		// Create random start positions for the pray and predator
-		generateCoordinates(pray, minBorder, maxBorder);
-		generateCoordinates(predator, minBorder, maxBorder);
+		generateCoordinates(pray, minBorder, n);
+		generateCoordinates(predator, minBorder, n);
 
 		// Printing the start positions
 		printPositions(pray, predator);
@@ -50,69 +44,68 @@ public class PredatorPray {
 		isMatchingPositions(pray, predator);
 
 		// Run through all moves to be performed
-		for (int i = 0; i < iterations; i++) {
-			// Check positions match 
+		for (int i = 0; i < t; i++) {
+			// Check positions match
 			isMatchingPositions(pray, predator);
-			
+
 			// Move predator
-			movePredator(predator, pray, maxMoveLength);
-			
+			movePredator(predator, pray, s);
+
 			// Move pray
-			movePray(pray, generateRandomInt(-maxMoveLength, maxMoveLength), minBorder, maxBorder);
-			
-			/* 
+			movePray(pray, generateRandomInt(-s, s), minBorder, n);
+
+			/*
 			 * We actually move the predator before the pray.
 			 * This is done as the predator moves in relation to the current position of the pray.
-			 * If we first call movePray, the predator would move towards the new coordinates 
-			 * of the pray instead, which is unwanted behavior in our program. 
+			 * If we first call movePray, the predator would move towards the new coordinates
+			 * of the pray instead, which is unwanted behavior in our program.
 			 */
-			
+
 			// Print current positions for pray and predator
 			printPositions(pray, predator);
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	/* parameterValidation:
-	 * Validates the given parameters. 
-	 * Note: We use the private keyword to encapsulate our methods we haven't 
-	 * planned to expose to other classes as this is best practice. 
+	 * Validates the given parameters.
+	 * Note: We use the private keyword to encapsulate our methods we haven't
+	 * planned to expose to other classes as this is best practice.
 	 */
 	private static void parameterValidation(final int maxBorder, final int maxMoveLength, final int iterations) {
-	
+
 		if ((maxBorder <= 0) || (maxMoveLength <= 0) || (iterations < 0)) {
 			System.out.println("Illegal Parameters!");
 			System.exit(0);
-		}	
-		
+		}
+
 	}
 
 
-	// TODO explain
 	private static void generateCoordinates(int[] animal, final int minBorder, final int maxBorder) {
 		for (int i = 0; i < animal.length; i++) {
 			animal[i] = generateRandomInt(minBorder, maxBorder);
 		}
 	}
-	
-	
+
+
 	/* generateRandomInt:
 	 * Pretty self-explanatory.
 	 * We use the Random.nextInt() instead of Math.random() to generate the random integer
-	 * as it both requires less processing and is "less biased" (but still negligible). 
-	 * https://stackoverflow.com/a/738651/360211 
+	 * as it both requires less processing and is "less biased" (but still negligible).
+	 * https://stackoverflow.com/a/738651/360211
 	 */
 	private static int generateRandomInt(int min, int max) {
 		// Credits: Greg Case (Stack Overflow)
 		return ThreadLocalRandom.current().nextInt(min, max + 1);
 	}
-	
-	
+
+
 	/* isMatchingPositions:
 	 * This method check to see if the current coordinates of the pray and predator matches.
-	 * The way we do this is to compare the two arrays and see if the content is identical. 
+	 * The way we do this is to compare the two arrays and see if the content is identical.
 	 * In the case they match, the program prints a confirmation and terminates.
 	 */
 	private static void isMatchingPositions(int[] pray, int[] predator) {
@@ -122,36 +115,33 @@ public class PredatorPray {
 			System.exit(1);
 		}
 	}
-	
-	
+
+
 	/* movePray:
-	 * TODO  
 	 */
-	private static int[] movePray(int[] pray, int randomInt, final int minBorder, final int maxBorder) {
+	private static void movePray(int[] pray, int randomInt, final int minBorder, final int maxBorder) {
 		// Pray moves by having a random value from [-s; s] added to both its coordinates
 		for (int i = 0; i < pray.length; i++) {
 			// Update value
 			pray[i] += randomInt;
 			// Check new value is inside border of grid
 			pray[i] = isInsideGrid(pray[i], minBorder, maxBorder);
-			
+
 		}
-		return pray;
 	}
-	
-	
+
+
 	/* movePredator:
-	 * TODO explain movePredator
 	 */
-	private static int[] movePredator(int[] predator, int[] pray, final int maxMoveLength) {
+	private static void movePredator(int[] predator, int[] pray, final int maxMoveLength) {
 
 		// Iterate through each coordinate position of the predator
 		for (int i = 0; i < predator.length; i++) {
-			
-			// Calculate the direction and distance 
+
+			// Calculate the direction and distance
 			int vectorDifference = pray[i] - predator[i];
 			int distance = Math.abs(vectorDifference);
-			
+
 			// Move the predator the optimal distance towards the pray
 			if (Math.abs(distance) <= maxMoveLength) {
 				predator[i] += vectorDifference;
@@ -165,15 +155,14 @@ public class PredatorPray {
 				}
 			}
 		}
-		
-		return predator;
+
 	}
-	
-	
+
+
 	/* isInsideGrid:
 	 * We actually only have to check if the pray is inside the box.
 	 * As long as the pray doesn't find a way to exit the Matrix (reference)
-	 * the predator can not escape as well, because of the implementation, 
+	 * the predator can not escape as well, because of the implementation,
 	 * that the predator never overshoots the prays coordinates.
 	 */
 	private static int isInsideGrid(int prayCoordinate, final int minBorder, final int maxBorder) {
@@ -186,8 +175,8 @@ public class PredatorPray {
 		}
 		return prayCoordinate;
 	}
-	
-	
+
+
 	/* printPositions:
 	 * Prints the current coordinates of the pray and then the predator.
 	 */
